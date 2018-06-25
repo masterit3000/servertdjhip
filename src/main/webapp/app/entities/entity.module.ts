a563adabea47e1e8410c3c73144decbaf5a2f374
masterit3000 <masterit3000@gmail.com>
2018-06-25 21:31:14 +0700

    first commit

--- src/main/webapp/app/entities/entity.module.ts
+++ src/main/webapp/app/entities/entity.module.ts
@@ -1,9 +1,41 @@
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
+import { ServertdjhipTinhModule } from './tinh/tinh.module';
+import { ServertdjhipHuyenModule } from './huyen/huyen.module';
+import { ServertdjhipXaModule } from './xa/xa.module';
+import { ServertdjhipKhachHangModule } from './khach-hang/khach-hang.module';
+import { ServertdjhipCuaHangModule } from './cua-hang/cua-hang.module';
+import { ServertdjhipNhanVienModule } from './nhan-vien/nhan-vien.module';
+import { ServertdjhipAnhKhachHangModule } from './anh-khach-hang/anh-khach-hang.module';
+import { ServertdjhipHopDongModule } from './hop-dong/hop-dong.module';
+import { ServertdjhipLichSuDongTienModule } from './lich-su-dong-tien/lich-su-dong-tien.module';
+import { ServertdjhipBatHoModule } from './bat-ho/bat-ho.module';
+import { ServertdjhipVayLaiModule } from './vay-lai/vay-lai.module';
+import { ServertdjhipGhiNoModule } from './ghi-no/ghi-no.module';
+import { ServertdjhipLichSuThaoTacHopDongModule } from './lich-su-thao-tac-hop-dong/lich-su-thao-tac-hop-dong.module';
+import { ServertdjhipThuChiModule } from './thu-chi/thu-chi.module';
+import { ServertdjhipTaiSanModule } from './tai-san/tai-san.module';
+import { ServertdjhipAnhTaiSanModule } from './anh-tai-san/anh-tai-san.module';
 /* jhipster-needle-add-entity-module-import - JHipster will add entity modules imports here */
 
 @NgModule({
     imports: [
+        ServertdjhipTinhModule,
+        ServertdjhipHuyenModule,
+        ServertdjhipXaModule,
+        ServertdjhipKhachHangModule,
+        ServertdjhipCuaHangModule,
+        ServertdjhipNhanVienModule,
+        ServertdjhipAnhKhachHangModule,
+        ServertdjhipHopDongModule,
+        ServertdjhipLichSuDongTienModule,
+        ServertdjhipBatHoModule,
+        ServertdjhipVayLaiModule,
+        ServertdjhipGhiNoModule,
+        ServertdjhipLichSuThaoTacHopDongModule,
+        ServertdjhipThuChiModule,
+        ServertdjhipTaiSanModule,
+        ServertdjhipAnhTaiSanModule,
         /* jhipster-needle-add-entity-module - JHipster will add entity modules here */
     ],
     declarations: [],
