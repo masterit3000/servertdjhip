a76c32c93f5dae8af3615b80b292506b33a9a2da
Zaffran <biprono12@gmail.com>
2018-06-26 18:11:11 +0700

    New

---
+++ src/main/webapp/app/entities/bat-ho/bat-ho-chuc-nang/bh-lich-dong-tien/bh-lich-dong-tien.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'jhi-bh-lich-dong-tien',
+  templateUrl: './bh-lich-dong-tien.component.html',
+  styles: []
+})
+export class BhLichDongTienComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
