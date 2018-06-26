7d2d48d2e0e01f57cfcdc7ce445c6ccfddd8c5ef
Zaffran <biprono12@gmail.com>
2018-06-26 17:35:02 +0700

    New

---
+++ src/main/webapp/app/entities/bat-ho/bat-ho-chuc-nang/bat-ho-chuc-nang.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'jhi-bat-ho-chuc-nang',
+  templateUrl: './bat-ho-chuc-nang.component.html',
+  styles: []
+})
+export class BatHoChucNangComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/main/webapp/app/entities/bat-ho/bat-ho.module.ts
+++ src/main/webapp/app/entities/bat-ho/bat-ho.module.ts
@@ -14,6 +14,7 @@ import {
     batHoRoute,
     batHoPopupRoute
 } from './';
+import { BatHoChucNangComponent } from './bat-ho-chuc-nang/bat-ho-chuc-nang.component';
 
 const ENTITY_STATES = [
     ...batHoRoute,
@@ -31,7 +32,8 @@ const ENTITY_STATES = [
         BatHoDialogComponent,
         BatHoDeleteDialogComponent,
         BatHoPopupComponent,
-        BatHoDeletePopupComponent
+        BatHoDeletePopupComponent,
+        BatHoChucNangComponent
     ],
     entryComponents: [
         BatHoComponent,
