b94f37d291f16f6ba1ebe09e87b4ae32c39bc1eb
Zaffran <biprono12@gmail.com>
2018-07-30 19:42:35 +0700

    Push

---
+++ src/main/webapp/app/entities/quan-ly/bat-ho/bat-ho-detail/bat-ho-detail.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'jhi-bat-ho-detail',
+  templateUrl: './bat-ho-detail.component.html',
+  styles: []
+})
+export class BatHoDetailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
