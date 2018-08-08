2c949c1ffd5abe4f953d65d293e1e23e9c900f88
unknown <thanhvuong6499@gmail.com>
2018-08-09 01:53:05 +0700

    bao cao thu lai bat ho vay lai

--- src/main/webapp/app/entities/bao-cao/chi-tiet-tien-lai/chi-tiet-tien-lai.component.ts
+++ src/main/webapp/app/entities/bao-cao/chi-tiet-tien-lai/chi-tiet-tien-lai.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { NhanVien, NhanVienService } from '../../nhan-vien';
+import { HttpResponse, HttpErrorResponse } from '../../../../../../../node_modules/@angular/common/http';
+import { JhiAlertService } from '../../../../../../../node_modules/ng-jhipster';
 
 @Component({
   selector: 'jhi-chi-tiet-tien-lai',
@@ -6,10 +9,30 @@ import { Component, OnInit } from '@angular/core';
   styles: []
 })
 export class ChiTietTienLaiComponent implements OnInit {
+  tungay: Date;
+  denngay: Date;
+  nhanVien: NhanVien;
+  nhanViens: NhanVien[];
+  constructor(
+    private nhanVienService: NhanVienService,
+    private jhiAlertService: JhiAlertService,
+  ) {
 
-  constructor() { }
+   }
 
   ngOnInit() {
+    this.loadNhanVien();
+  }
+  loadNhanVien() {
+    this.nhanVienService.query().subscribe(
+      (res: HttpResponse<NhanVien[]>) => {
+        this.nhanViens = res.body;
+      },
+      (res: HttpErrorResponse) => this.onError(res.message)
+    );
+  }
+  private onError(error) {
+    this.jhiAlertService.error(error.message, null, null);
   }
 
 }
