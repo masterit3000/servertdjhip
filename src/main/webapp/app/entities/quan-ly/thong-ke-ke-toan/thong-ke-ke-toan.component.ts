04eb73d6451239ebde2bec469308f61eb1e25668
Zaffran <biprono12@gmail.com>
2018-09-26 13:26:35 +0700

    Thống kê + báo cáo kế toán

---
+++ src/main/webapp/app/entities/quan-ly/thong-ke-ke-toan/thong-ke-ke-toan.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'jhi-thong-ke-ke-toan',
+  templateUrl: './thong-ke-ke-toan.component.html',
+  styles: []
+})
+export class ThongKeKeToanComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
